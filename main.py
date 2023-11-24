7cb71599d2972d83a16d2a6e4e642f7a6eb3c0f0
Jhonnatan Espinoza Rojas <hytzy18@gmail.com>
2023-11-24 05:44:50 -0500

    se soluciono problema de asincronismo

--- main.py
+++ main.py
@@ -53,9 +53,12 @@
 import socket
 import random
 import threading
-import select
 import time
 import queue
+import asyncio
+import websockets
+import json
+import uuid
 
 # ------------------------------------
 # Class to create a client
@@ -74,8 +77,7 @@ class ClientConnection:
 
     # Send a message to the node
     def send(self, message):
-        # message = message + '\n'
-        self.connection.send(message.encode('utf-8'))
+        self.connection.sendall(message.encode('utf-8'))
 
     # Receive a message from the node
     def receive(self):
@@ -87,52 +89,13 @@ class ClientConnection:
 
 
 # ------------------------------------
-# Class to create a server
-# ------------------------------------
-class ServerCreation:
-
-    # Constructor
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.server = None
-
-    # Listen for incoming connections
-    def listen(self, queue_size):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen(queue_size)
-
-    # Accept a connection from a client
-    def accept(self):
-        self.connection, self.address = self.server.accept()
-        return self.connection, self.address
-
-    # Send a message to the client
-    def send(self, message):
-        # message = message + '\n'
-        self.connection.send(message.encode('utf-8'))
-
-    # Receive a message from the client
-    def receive(self):
-        return self.connection.recv(1024).decode('utf-8')
-
-    # Close the connection with the client
-    def close(self):
-        self.connection.close()
-
-    def fileno(self):
-        return self.server.fileno()
-
-
-# ------------------------------------
 # Configuration of the nodes
 # ------------------------------------
 master_node = {'address':'localhost', 'port':40000}
 slave_nodes = [
     {'address':'localhost', 'port':50000}
 ]
-forwarder_node = {'address':'localhost', 'port':60000, 'queue_size':1}
+forwarder_node = {'address':'localhost', 'port':60000}
 
 
 # ------------------------------------
@@ -141,7 +104,10 @@ forwarder_node = {'address':'localhost', 'port':60000, 'queue_size':1}
 queue_reads = queue.Queue()
 queue_transactions = queue.Queue()
 queue_response = queue.Queue()
-clients = {}
+queries_dict = []
+
+index_query = 1
+validation = []
 
 # ------------------------------------
 # Function to get a random index
@@ -149,6 +115,14 @@ clients = {}
 # Function receives a size and returns a random index
 def get_random_index(size):
     return random.randint(0, size - 1)
+
+
+# ------------------------------------
+# Function to get a unique id
+# ------------------------------------
+def get_unique_id(index_query):
+    index_query += 1
+    return index_query
 
 
 # ------------------------------------
@@ -164,15 +138,15 @@ def send_all_slaves(message, list_slaves):
         list_slaves[i].send(message)
         print(f"Mensaje enviado al nodo Slave: {str(slave_nodes[i])}")
 
+
 # ------------------------------------
 # Function to find a client socket
 # ------------------------------------
-def find_client_socket(clients, user_ip, user_port):
-    for client_socket, address_client in clients.items():
-        if address_client[0] == user_ip and address_client[1] == user_port:
-            return client_socket
+def find_client_socket(query_id):
+    for index in range(len(queries_dict)):
+        if queries_dict[index]['id'] == int(query_id):
+            return queries_dict[index]['client']
     return None
-
 
 # ------------------------------------
 # Function to handle the communication with the master node
@@ -195,7 +169,7 @@ def handle_master_response(master_connection, list_slaves):
                 print("[A] Se recibió una transacción...")
                 queue_response.put(response)
                 aux = response.split('-')
-                response = f"{aux[0]}-{aux[2]}-{aux[3]}-{aux[4]}"
+                response = f"{aux[0]}-{aux[2]}-{aux[3]}-{aux[4]}-{aux[5]}"
                 send_all_slaves(response, list_slaves)
             # Master node sends the hash
             elif response.startswith("M"):
@@ -210,7 +184,7 @@ def handle_master_response(master_connection, list_slaves):
                 print("[F] Se recibió un error...")
             # Unknown message
             else:
-                print(f"Respuesta desconocida: {response}")
+                print(f"Respuesta desconocida: {response} desde el nodo Master: {str(master_connection)}")
 
 
 # ------------------------------------
@@ -219,7 +193,7 @@ def handle_master_response(master_connection, list_slaves):
 # Function receives a slave connection, a master connection and a list of connections
 # and handles the communication with the slave node
 def handle_slave_response(slave_connection, master_connection, list_slaves):
-    message_saved = ''
+    nonce_found = ''
     # Loop to receive messages from the slave node
     while True:
         # Receive message from the slave node
@@ -234,20 +208,73 @@ def handle_slave_response(slave_connection, master_connection, list_slaves):
             elif response.startswith("M"):
                 print("[M] Se encontró el nonce...")
                 # Send the message to all the slaves with mode V
-                send_all_slaves(response.replace("M", "V", 1), list_slaves)
+                nonce_found = response.replace("M", "V", 1)
+                send_all_slaves(nonce_found, list_slaves)
             # Slave node sends the validation
             elif response.startswith("V"):
                 print("[V] Un nodo slave valido el hash ...")
-                print(f"El slave envio: {response.split('-')[1]}")
-                if response.split('-')[1] == 'True':
-                    master_connection.send(message_saved)
-                    message_saved = ''
+                validation_response = response.split('-')[1]
+                if validation_response == 'True':
+                    validation.append(True)
+                else:
+                    validation.append(False)
+                if validation.count(True) == len(list_slaves):
+                    print("Se valido el nonce...")
+                    percentage = validation.count(True)/len(list_slaves)
+                    master_connection.send(f"{nonce_found}-{percentage}\n")
+                    validation.clear()
             # Slave node sends the hash
             elif response.startswith("F"):
                 print("[F] Se recibió un error...")
             # Unknown message
             else:
-                print(f"Respuesta desconocida: {response}")
+                print(f"Respuesta desconocida: {response} desde el nodo Slave: {str(slave_connection)}")
+
+
+# ------------------------------------
+# Function handle client (WebSocket)
+# ------------------------------------
+
+async def handle_client(websocket):
+    # Add your WebSocket handling logic here
+    print(f"Cliente conectado desde {websocket.remote_address}")
+    # Loop to receive messages from the client
+    async for message in websocket:
+        try:
+            # Process the received message as needed
+            data = json.loads(message)
+            if data['type'] == 'L':
+                # Extract the necessary information
+                mode = data['type']
+                origin_account = data['origin_account']
+                query_id = get_unique_id(index_query)
+                # Create the query read
+                read = f"{mode}-{query_id}-{origin_account}\n"
+                print(f"Recibe consulta {query_id} de lectura para la cuenta {origin_account}")
+                # Add the query to the list of queries
+                queries_dict.append({'id': query_id, 'client': websocket})
+                # Add message to the queue of reads
+                queue_reads.put(read)
+            elif data['type'] == 'A':
+                # Extract the necessary information
+                mode = data['type']
+                origin_account = data['origin_account']
+                destiny_account = data['destination_account']
+                amount = data['amount']
+                query_id = get_unique_id(index_query)
+                # Create the query transaction
+                transaction = f"{mode}-{query_id}-{origin_account}-{destiny_account}-{amount}\n"
+                print(f"Recibe transaccion {query_id} de {amount} desde acc:{origin_account} hacia acc:{destiny_account}")
+                # Add the query to the list of queries
+                queries_dict.append({'id': query_id, 'client': websocket})
+                # Add message to the queue of transactions
+                queue_transactions.put(transaction)
+            else:
+                # Message unknown
+                print(f"Respuesta desconocida: {data} desde el cliente: {websocket.remote_address}")
+        except Exception as e:
+            print(f"Error al procesar el mensaje: {e}")
+
 
 # ------------------------------------
 # Function to handle the queues
@@ -265,31 +292,52 @@ def handle_queues(master_connection, slaves_list):
             random_index = get_random_index(len(slaves_list))
             # Send message to the random slave node
             slaves_list[random_index].send(message_from_client)
+            print(f"Se envio la consulta al eslave[{random_index}]")
         # Verify if the queue of transactions is not empty
         if not queue_transactions.empty():
             # Get message from the queue of transactions
             message_from_client = queue_transactions.get()
             # Send message to the master node
             master_connection.send(message_from_client)
+            print(f"Se envio la consulta de actualizacion al master")
+        # Wait 100 ms to check again
+        time.sleep(0.1)
+
+async def handle_server_forwarder():
+    # Loop to check if the queues are not empty
+    while True:
         if not queue_response.empty():
             # Get message from the queue of responses
             message_to_client = queue_response.get()
-            print(f"Usuario: {message_to_client.split('-')[2]} - Balance: {message_to_client.split('-')[3]}")
             # Get the ip and port of the client
-            user_id = message_to_client.split('-')[1]
-            user_ip, user_port = user_id.split(':')
+            query_id = message_to_client.split('-')[1]
+            print(f"Se encontro una respuesta para {query_id}...")
             # Find the client socket corresponding to the user's IP and port
-            client_socket = find_client_socket( clients, user_ip, int(user_port))
+            client_websocket = find_client_socket(query_id)
             # If the client socket was found
-            if client_socket:
+            if client_websocket:
                 # Send message to the client
-                client_socket.send(message_to_client.encode('utf-8'))
-                print(f"Mensaje enviado al cliente: {message_to_client}")
+                mode = message_to_client.split('-')[0]
+                json_response = ''
+                if mode == 'L':
+                    id_client = message_to_client.split('-')[1]
+                    origin_account = message_to_client.split('-')[2]
+                    origin_amount = message_to_client.split('-')[3]
+                    json_response = json.dumps({'type': mode, 'id': id_client, 'origin_account': origin_account, 'origin_amount': origin_amount})
+                elif mode == 'A':
+                    id_client = message_to_client.split('-')[1]
+                    origin_account = message_to_client.split('-')[2]
+                    origin_amount = message_to_client.split('-')[3]
+                    destiny_account = message_to_client.split('-')[4]
+                    destiny_amount = message_to_client.split('-')[5]
+                    json_response = json.dumps({'type': mode, 'id': id_client, 'origin_account': origin_account, 'origin_amount': origin_amount, 'destiny_account': destiny_account, 'destiny_amount': destiny_amount})
+                await client_websocket.send(json_response)
+                print("Mensaje enviado al cliente")
             else:
-                print(f"No se encontró el cliente correspondiente a {user_id}")
-
+                print(f"No se encontró el cliente asignado al id {query_id}")
         # Wait 100 ms to check again
         time.sleep(0.1)
+
 
 # ------------------------------------
 # Function main
@@ -299,7 +347,6 @@ def main():
 
     # Create a socket to connect to the master node
     master_connection = ClientConnection(master_node['address'], master_node['port'])
-
     # Connect to the master node
     master_connection.connect()
     print(f"Conectado al nodo Master {str(master_node)}")
@@ -308,94 +355,50 @@ def main():
 
     # Get the size of the list of slaves
     size = len(slave_nodes)
-
     # Create a list of connections to the slave nodes
     slaves_list = []
-
     # Connect to all the slave nodes
     for i in range(size):
         # Create a socket to connect to the slave node
         slave_connection = ClientConnection(slave_nodes[i]['address'], slave_nodes[i]['port'])
-
         # Connect to the slave node
         slave_connection.connect()
         print(f"Conectado al nodo Slave: {str(slave_nodes[i])}")
-        
         # Add the connection to the list of connections
         slaves_list.append(slave_connection)
 
-    # ---------- Send accounts to all slaves ----------
-
-    # Execute mode C to get the accounts
-    master_connection.send("C")
-
     # ---------- Threads creation ----------
+    # Iniciar un bucle asyncio
+    loop = asyncio.get_event_loop()
 
-    # Create a thread to handle the communication with the master node
+    # Crear y ejecutar un hilo para la función asyncio
+    asyncio_thread = threading.Thread(target=run_asyncio_loop, args=(loop,))
+    asyncio_thread.start()
+
     response_thread_master = threading.Thread(target=handle_master_response, args=(master_connection,slaves_list))
     response_thread_master.start()
 
-    # Create a thread to handle the communication with the slave nodes
-    for i in range(size):
-        response_thread_slave = threading.Thread(target=handle_slave_response, args=(slaves_list[i],master_connection,slaves_list))
+    for i in range(len(slaves_list)):
+        response_thread_slave = threading.Thread(target=handle_slave_response, args=(slaves_list[i], master_connection, slaves_list))
         response_thread_slave.start()
 
-    # Create a thread to handle the queues
     queue_thread = threading.Thread(target=handle_queues, args=(master_connection, slaves_list))
     queue_thread.start()
+    
 
-    # ---------- Server Forwarder creation ----------
 
-    # Create a server to listen for incoming connections
-    server_forwarder = ServerCreation(forwarder_node['address'], forwarder_node['port'])
-    server_forwarder.listen(forwarder_node['queue_size'])
-    print("Esperando conexiones entrantes...")
+def run_asyncio_loop(loop):
+    loop.run_until_complete(main_server_forwarder())
 
-    # List of sockets for select.select()
-    sockets_list = [server_forwarder]
 
-    # ---------- Client nodes connection ----------
+async def main_server_forwarder():
 
-    while True:
-        # Wait for an activity to happen
-        read_sockets, _, _ = select.select(sockets_list, [], [])
-
-        for sock in read_sockets:
-            # New connection
-            if sock == server_forwarder:
-                # Accept the connection from the client
-                client_socket, address_client = server_forwarder.accept()
-                # Add the connection to the list of sockets
-                sockets_list.append(client_socket)
-                # Add the client to the list of clients
-                clients[client_socket] = address_client
-                print(f"Conexión establecida desde {address_client}")
-
-            # Message from a client
-            else:
-                try:
-                    # Receive message from the client
-                    message = sock.recv(1024).decode("utf-8")
-                    if message:
-                        if message.startswith("L"):
-                            print(f"Procesar modo L: {message}")
-                            # Add message to the queue of reads
-                            queue_reads.put(message)
-                        elif message.startswith("A"):
-                            print(f"Procesar modo A: {message}")
-                            # Add message to the queue of transactions
-                            queue_transactions.put(message)
-                        else:
-                            # Message unknown
-                            print(f"Respuesta desconocida: {message}")
-                except Exception as e:
-                    print(f"Cliente {clients[sock]} desconectado...")
-                    sock.close()
-                    sockets_list.remove(sock)
-                    del clients[sock]
-                    continue
+    async with websockets.serve(handle_client, forwarder_node['address'], forwarder_node['port']):
+        print(f"WebSocket Server Forwarder escuchando: {forwarder_node}")
+        await asyncio.Future()
 
 if __name__ == "__main__":
     main()
+    asyncio.run(handle_server_forwarder())
 
-# python3 main.py
+# python main.py
